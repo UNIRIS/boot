84c2f472c04b0b3865cb49c850aa3aeece0478a0
Samuel <samuel@uniris.io>
2021-03-30 15:41:19 +0200

    Open SSH using UPnP

--- prepare.sh
+++ prepare.sh
@@ -2,3 +2,14 @@
 
 # Prevent the node to sleep
 sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# Install tool to open UPnP
+sudo apt-get update
+sudo apt install miniupnpc
+
+# Open SSH port with UPnP
+LOCAL_IP=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+upnpc -c $LOCAL_IP 22 22 TCP
+
+# Get Public IP
+PUBLIC_IP=$(upnpc -s | grep -Po 'ExternalIPAddress = \K(.*)')
