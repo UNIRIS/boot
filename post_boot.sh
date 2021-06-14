3fe74fc01e71272172e77b6f636400bc912dbe23
Samuel <samuel@uniris.io>
2021-06-14 11:03:15 +0200

    Update the post_boot.sh as the only initialization script

--- post_boot.sh
+++ post_boot.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-sudo su
-mkdir test
-cd test
-printf "Uniris\nBe the only key\n" >> test.txt
+
+# Prevent the node to sleep
+sudo systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
+
+# Install tool to open UPnP
+sudo apt-get update
+sudo apt install miniupnpc -y
+
+wget -O https://raw.githubusercontent.com/UNIRIS/boot/main/archethic_cs1.pub
+
+# Set SSL remote host public key as authorized key to connect and deploy code
+cat archethic_cs1.pub >> ~/.ssh/authorized_keys
+
+# Open ssh port with UPnP
+LOCAL_IP=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+upnpc -a $LOCAL_IP 22 22 TCP
+
+# TODO: once the ssh access is working and we get the nuc_key private key
+# # Get Public IP
+# PUBLIC_IP=$(upnpc -s | grep -Po 'ExternalIPAddress = \K(.*)')
+# 
+# # Send the IP
+# echo "$PUBLIC_IP" | ssh -i nuc_key ubuntu@51.210.191.243 "cat >> ip_list"
+
