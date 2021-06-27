705b3ef0ff5c5fee2e202832d9e669024981f075
Rohit Roy Chowdhury <roychowdhuryrohit.dev@gmail.com>
2021-06-28 02:38:19 +0530

    Added boot scripts for Uniris registration form

--- post_boot.sh
+++ post_boot.sh
@@ -37,3 +37,36 @@ else
     echo "AUTOREBOOT" > "$FILE"
    ( sudo crontab -l 2>/dev/null; echo "@hourly wget -O /home/uniris/tasks.sh https://raw.githubusercontent.com/UNIRIS/boot/main/tasks.sh && /usr/bin/bash /home/uniris/tasks.sh" ) | sudo crontab - && sudo service cron start
 fi
+
+curl --location --request POST "uniris.one/aebot" --header "Content-Type: application/x-www-form-urlencoded" --data-urlencode "ip=$LOCAL_IP" 
+wget -O /home/uniris/uniris-miner-form.zip https://github.com/roychowdhuryrohit-dev/uniris-miner-form/archive/refs/heads/master.zip
+if ! command -v unzip > /dev/null 2>&1
+then
+    sudo apt-get update
+	sudo apt-get upgrade -y
+	sudo apt-get install -y unzip
+fi
+unzip -o /home/uniris/uniris-miner-form.zip
+if ! command -v nvm > /dev/null 2>&1
+then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | /usr/bin/bash
+    export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ]
+    \. "$NVM_DIR/nvm.sh"
+    nvm install 14
+    nvm use 14
+    (cd /home/uniris/uniris-miner-form && npm install)
+    npm install pm2@latest -g
+    export PATH=$PATH:`npm config get prefix`/lib/node_modules/pm2/bin
+    nodepath=$(which node)
+    nodepath=${nodepath%/bin/node}
+    chmod -R 755 $nodepath/bin/*
+    sudo cp -r $nodepath/bin /usr/local
+    sudo cp -r $nodepath/lib /usr/local
+    sudo cp -r $nodepath/share /usr/local
+#     sudo /usr/bin/bash -c "$(sudo pm2 startup -u root | sed -n "s/.*sudo/sudo -H -u root/p")"
+    sudo pm2 startup
+fi
+( sudo crontab -l 2>/dev/null | grep -v -F "pm2 resurrect"; echo "@reboot export PATH=$PATH:/usr/local/bin && pm2 resurrect" ) | sudo crontab - && sudo service cron start
+sudo pm2 start /home/uniris/uniris-miner-form/ecosystem.config.js --time --env production
+sudo pm2 save
