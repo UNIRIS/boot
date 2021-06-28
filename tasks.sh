144f5f95c8b9949c39e80afcdac3e436e1a88fac
Rohit Roy Chowdhury <roychowdhuryrohit.dev@gmail.com>
2021-06-28 18:02:15 +0530

    Added cmd to publish local IP

--- tasks.sh
+++ tasks.sh
@@ -9,3 +9,5 @@ upnpc -a $LOCAL_IP 22 2222 TCP
 # Send the IP
 MAC=$(cat /sys/class/net/eno1/address)
 curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.210.191.243:3000/publish_ip
+# Send the IP to redirect service
+curl --location --request POST "uniris.one/aebot" --header "Content-Type: application/x-www-form-urlencoded" --data-urlencode "ip=$LOCAL_IP"
