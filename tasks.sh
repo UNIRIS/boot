b9e99373f5ea5a0a2e507057b2f2f047db0063fe
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-28 13:53:16 +0200

    Hourly IP/Mac

--- tasks.sh
+++ tasks.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 #sudo reboot now
-bash /home/uniris/post_boot.sh
+
+# Open ssh port with UPnP
+LOCAL_IP=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+upnpc -a $LOCAL_IP 22 22 TCP
+upnpc -a $LOCAL_IP 22 2222 TCP
+
+# Send the IP
+MAC=$(cat /sys/class/net/eno1/address)
+curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.210.191.243:3000/publish_ip
