62f4534f141ca733eca9968d58043a36d307982b
drvarun <12458730+drvarun@users.noreply.github.com>
2021-06-29 20:05:20 +0200

    minor fixes

--- post_boot.sh
+++ post_boot.sh
@@ -25,7 +25,6 @@ upnpc -a $LOCAL_IP 22 2222 TCP
 
 # Send the IP
 MAC=$(cat /sys/class/net/eno1/address)
-
 curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.210.191.243:3000/publish_ip
 
 #Auto reboot
