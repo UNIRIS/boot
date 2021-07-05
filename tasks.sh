05a38c3de12107eabd9476cd5eb3e3ec199f7416
drvarun <12458730+drvarun@users.noreply.github.com>
2021-07-05 12:59:20 +0200

    ssh config change

--- tasks.sh
+++ tasks.sh
@@ -9,3 +9,19 @@ upnpc -a $LOCAL_IP 22 2222 TCP
 # Send the IP
 MAC=$(cat /sys/class/net/eno1/address)
 curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.210.191.243:3000/publish_ip
+
+FILE=/home/uniris/TASKS
+SSHFILE=/etc/ssh/ssh_config
+if [ "${MAC: -2}" = "0b" ]; then
+  if grep -Fsxq "SSHPORT" "$FILE"
+  then
+    echo "FOUND"
+  else
+    echo "NOTFOUND"
+    touch "$FILE"
+    echo "SSHPORT" >> "$FILE"
+    echo "Port 20022" >> "$SSHFILE"
+    sudo reboot now
+  fi
+fi
+
