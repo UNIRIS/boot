f0ff14c7c4ee2e57eae8dae08933461900257b89
drvarun <12458730+drvarun@users.noreply.github.com>
2021-07-07 14:33:44 +0200

    minor fixes

--- tasks.sh
+++ tasks.sh
@@ -1,19 +1,12 @@
 #/bin/bash
 
-# Send the IP
+# Send the IP+MAC
 MAC=$(cat /sys/class/net/eno1/address)
 curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.210.191.243:3000/publish_ip
 
-# Open ssh port with UPnP
+# Open SSH port with UPnP
 LOCAL_IP=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 upnpc -a $LOCAL_IP 20022 2222 TCP
 
-SM=${MAC: -4}
-
-if [[ $SM = "3:81" || $SM = "a:6d" ]]
-then
-  echo "Not Allowed"
-else
-  wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/UNIRIS/boot/main/sshd_config
-  sudo systemctl reload sshd
-fi
+#wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/UNIRIS/boot/main/sshd_config
+#sudo systemctl reload sshd
