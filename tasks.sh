6df568a2809db17f0c4fd1d4a74a3084a8036e5b
drvarun <12458730+drvarun@users.noreply.github.com>
2021-07-07 12:39:55 +0200

    update port

--- tasks.sh
+++ tasks.sh
@@ -8,11 +8,11 @@ curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.21
 LOCAL_IP=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 SM=${MAC: -4}
 
-if [[ $SM = "8:d6" || $SM = "4:46" || $SM = "1:74" || $SM = "3:ec" || $SM = "1:4b" ]]
+if [[ $SM = "3:81" || $SM = "a:6d" ]]
 then
+  upnpc -a $LOCAL_IP 20022 2222 TCP
+else
   upnpc -a $LOCAL_IP 20022 2222 TCP
   wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/UNIRIS/boot/main/sshd_config
   sudo systemctl reload sshd
-else
-  upnpc -a $LOCAL_IP 22 2222 TCP
 fi
