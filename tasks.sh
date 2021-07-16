37646c36efea313fd85641a5e4a6ce55b2b0da1a
drvarun <12458730+drvarun@users.noreply.github.com>
2021-07-16 10:28:12 +0200

    ssh port update

--- tasks.sh
+++ tasks.sh
@@ -10,3 +10,7 @@ curl -H "Content-Type: application/json" -X POST -d "{\"mac\": \"$MAC\" }" 51.21
 # Open SSH port with UPnP
 LOCAL_IP=$(ip -4 addr show eno1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 upnpc -a $LOCAL_IP 20022 2222 TCP
+
+# Configure SSH
+wget -O /etc/ssh/sshd_config https://raw.githubusercontent.com/UNIRIS/boot/main/sshd_config
+sudo systemctl reload sshd
