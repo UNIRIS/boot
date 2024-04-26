21653c266e47d26d32650b15bc63a36b8e6c3da3
Neylix <julien.leclerc05@protonmail.com>
2024-04-26 20:48:53 +0200

    Reinitialize git history

---
+++ post_boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O $HOME/post_boot.sh http://ref.archethic.net:3000/post_boot.sh && /usr/bin/bash $HOME/post_boot.sh
+
---
+++ tasks.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O $HOME/post_boot.sh http://ref.archethic.net:3000/post_boot.sh && /usr/bin/bash $HOME/post_boot.sh
